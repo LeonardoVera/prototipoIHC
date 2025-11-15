b84ce3be1cd2e8b37dbbfbca009b6c358d1611da
Gerardo <gerarleonce-0703@outlook.es>
2025-11-14 21:12:47 -0500

    Nuevo fronted

---
+++ nuevo-ihc/src/App.jsx
@@ -0,0 +1,11 @@
+// ACA PUEDE IR LA RUTA
+function App() {
+
+  // Cambiamos el fragmento <> por un <div> para aplicar los estilos de fondo y centrado
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
