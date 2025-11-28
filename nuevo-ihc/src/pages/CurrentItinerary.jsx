0e12e3a6676219e1422a857d7b31bea052058bb4
Angel <angel.carrasco@unmsm.edu.pe>
2025-11-28 17:09:09 -0500

    fin

--- nuevo-ihc/src/pages/CurrentItinerary.jsx
+++ nuevo-ihc/src/pages/CurrentItinerary.jsx
@@ -63,24 +63,6 @@ export default function CurrentItinerary() {
         </div>
 
         </div>
-
-        {/* Botón flotante para compartir */}
-        <button className="absolute bottom-6 right-6 bg-teal-600 text-white p-4 rounded-full shadow-2xl hover:bg-teal-700 transition-colors z-30">
-          <svg 
-            xmlns="http://www.w3.org/2000/svg" 
-            className="h-6 w-6" 
-            fill="none" 
-            viewBox="0 0 24 24" 
-            stroke="currentColor"
-          >
-            <path 
-              strokeLinecap="round" 
-              strokeLinejoin="round" 
-              strokeWidth={2} 
-              d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" 
-            />
-          </svg>
-        </button>
       </div>
 
       <BottomSheet
